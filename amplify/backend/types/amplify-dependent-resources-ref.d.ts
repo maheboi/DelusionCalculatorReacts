9b42e9cce9e1b9f1a8b77c6de526591854ec7941
maheboi <136125555+maheboi@users.noreply.github.com>
2023-06-23 21:12:30 -0500

    Added DB and API
    Added the db and api

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "calculatorreacts": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './Bag.png';
 import './App.css';
 
 function App() {
